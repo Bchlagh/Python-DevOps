8afe50d94cb240973e0f48de4cdd44879b4996a2
Bchlagh <chlaghme@gmail.com>
2023-12-20 12:34:21 +0100

    l'ajout de fichier

--- projet2/parse-json.py
+++ projet2/parse-json.py
@@ -1,34 +1,12 @@
-import os
 import json
+import os
 
-def parse_json(file_path):
-    # Déterminer et afficher le chemin du fichier JSON
-    print(f"Chemin du fichier JSON : {file_path}")
-
-    # Créer un chemin absolu vers le fichier JSON
-    abs_file_path = os.path.abspath(file_path)
-    print(f"Chemin absolu du fichier JSON : {abs_file_path}")
-
-    try:
-        # Lire et analyser le fichier JSON
-        with open(file_path, 'r') as json_file:
-            data = json.load(json_file)
-
-            # Extrait et affiche la valeur associée à la clé 'name'
-            if 'name' in data:
-                print(f"Valeur associée à la clé 'name' : {data['name']}")
-
-            # Parcourt toutes les clés et valeurs du dictionnaire JSON
-            print("Toutes les paires clé-valeur dans le fichier JSON:")
-            for key, value in data.items():
-                print(f"{key}: {value}")
-
-    except FileNotFoundError:
-        print("Le fichier JSON n'a pas été trouvé.")
-    except json.JSONDecodeError:
-        print("Erreur de décodage JSON. Assurez-vous que le fichier est au format JSON valide.")
-
-if __name__ == "__main__":
-    # Remplacez le chemin du fichier JSON par le chemin réel de votre fichier
-    json_file_path = "chemin/vers/votre/fichier.json"
-    parse_json(json_file_path)
+script_dir = os.path.dirname(__file__)
+print("le script est dans  : " + script_dir)
+script_absolute_path = os.path.join(script_dir, 'files/example.json')
+json = json.loads(open(script_absolute_path).read())
+value = json['name']
+print(value)
+for key in json:
+    value = json[key]
+    print("la clé et la valeur sont  ({}) = ({})".format(key, value))
